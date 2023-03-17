3b8179edefea8fe3da3b2935fa78ef9dbeb47ad0
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2023-03-17 11:52:33 +0200

    Reduce complexity in actionPerformed method and solve some problems

--- src/edu/najah/cap/ex/EditorException.java
+++ src/edu/najah/cap/ex/EditorException.java
@@ -1,14 +1,8 @@
 package edu.najah.cap.ex;
 
-import java.nio.file.NoSuchFileException;
-
 public class EditorException extends Exception {
-private final ExceptionType exceptionType;
-    public EditorException(String message,ExceptionType exceptionType) {
+    public EditorException(String message) {
         super(message);
-        this.exceptionType=exceptionType;
     }
-    public ExceptionType getExceptionType(){
-        return exceptionType;
-    }
+
 }
